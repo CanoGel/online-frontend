b486993ecefdb4609e15b0e8fea0f5fb1e2be0fd
CanoGel <ahmadisse23@gmail.com>
2025-07-02 02:13:47 +0300

    Last Update

--- src/utils/imageUrl.jsx
+++ src/utils/imageUrl.jsx
@@ -2,6 +2,6 @@ export function getImageUrl(image) {
   if (!image) return '/images/book-placeholder.jpg';
   if (image.startsWith('/images/')) return image;
   if (image.startsWith('http')) return image;
-  // If image is a backend path, prepend backend URL
+  if (!image.startsWith('/')) image = '/' + image;
   return `https://online-book-backend.onrender.com${image}`;
 }
